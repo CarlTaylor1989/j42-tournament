89a86b139752fcea54141ff967d2b65f6bde9e8d
CarlTaylor1989 <carltaylor1989@gmail.com>
2018-06-07 14:45:43 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  private apiURL = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Acolos?api_key=RGAPI-10b19e19-accf-4a4e-8739-f67e2287d4e3';
+  data: any = {};
+
+  constructor(private http: Http) {
+    this.getData();
+    this.getSummonerInfo();
+  }
+
+  getData() {
+    return this.http.get(this.apiURL).map((res: Response) => res.json());
+  }
+
+  getSummonerInfo() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MzButtonModule, MzBadgeModule, MzProgressModule, MzCollectionModule, MzDropdownModule, MzSidenavModule } from 'ngx-materialize';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MzButtonModule,
+    MzBadgeModule,
+    MzProgressModule,
+    MzCollectionModule,
+    MzDropdownModule,
+    MzSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
